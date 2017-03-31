564c8e492233d4238a57268e28708f8a78e0a70a
Shay Zambrovski <shaike.zam@gmail.com>
2017-03-31 20:41:05 +0300

    [FEATURE] user turn

--- game.js
+++ game.js
@@ -1,19 +1,21 @@
-(function(arg1, arg2, arg3) {
-
+(function(arg1, click, arg3) {
     var initBoardGame = function(sPlayer1Name, sPlayer2Name) {
+        $("td").on(click, function(oEvent) {
+            console.log(oEvent.target.getAttribute("name"));
+        }.bind(this))
         this.game = {
             player: [sPlayer1Name, sPlayer2Name],
             gameBoard: {
-                r0: [],
-                r1: [],
-                r2: [],
-                r3: [],
-                r4: [],
-                r5: [],
-                r6: []
+                r0: [null, null, null, null, null, null, null],
+                r1: [null, null, null, null, null, null, null],
+                r2: [null, null, null, null, null, null, null],
+                r3: [null, null, null, null, null, null, null],
+                r4: [null, null, null, null, null, null, null],
+                r5: [null, null, null, null, null, null, null],
+                r6: [null, null, null, null, null, null, null]
             }
         }
     }.bind(this)
     initBoardGame("A1", "B2")
     console.log(this.game);
-}(document, "getElementBy", "oncClick"))
+}(document, "click"))
