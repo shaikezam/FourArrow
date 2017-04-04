c5f2791b8aa25636fc0dab74a7b8b19af433228b
i320062 <shay.zambrovski@sap.com>
2017-04-04 09:00:56 +0300

    [FEATURE] integration with karma

--- test/test.js
+++ test/test.js
@@ -1,8 +1,7 @@
-var assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal(-1, [1, 2, 3].indexOf(4));
-        });
-    });
+QUnit.test('Name of Test', function(assert) {
+    // Setup the various states of the code you want to test and assert conditions.
+    assert.equal(1, 1, '1 === 1');
+    assert.ok(true, 'true is truthy');
+    assert.ok(1, '1 is also truthy');
+    assert.ok([], 'so is an empty array or object');
 });
