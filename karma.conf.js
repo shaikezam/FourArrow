c3a72baf96ec81f89ca2302d0b22241ff8b8f2e4
Shay Zambrovski <shaike.zam@gmail.com>
2017-04-05 18:52:54 +0300

    [FIX] add require

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'game/*.html',
-            'scripts/app.js',
+            'game/*html',
+            'scripts/*js',
             'test/*js'
         ],
 
